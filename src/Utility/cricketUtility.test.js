e38773f01101ed15fe7018c6cb78192c11266401
leena-gokhale <gokhale.leena@gmail.com>
2018-08-28 11:30:31 +0530

    es lint changes

--- src/Utility/cricketUtility.test.js
+++ src/Utility/cricketUtility.test.js
@@ -1,15 +1,12 @@
-import { CricketUtility } from "./cricketUtility";
+import { CricketUtility } from './cricketUtility';
 
-describe("cricketUtility", function() {
-
-    it("Expects Whole Number when over is complete", function() {
-         const overs =  CricketUtility.getOversFromTotalBallsPlayed(6);
-         expect(overs).toEqual(1);
-    }) 
-
-    it("Expects Over in decimal format when over is still in progress", function() {
-        const overs =  CricketUtility.getOversFromTotalBallsPlayed(4);
-        expect(overs).toEqual("0.4");
-   }) 
-
-})
+describe('cricketUtility', () => {
+  it('Expects Whole Number when over is complete', () => {
+    const overs = CricketUtility.getOversFromTotalBallsPlayed(6);
+    expect(overs).toEqual(1);
+  });
+  it('Expects Over in decimal format when over is still in progress', () => {
+    const overs = CricketUtility.getOversFromTotalBallsPlayed(4);
+    expect(overs).toEqual('0.4');
+  });
+});
