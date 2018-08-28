2b76e27c9809e20bedc0e69bb0433fce637a4dbf
Nayanika <in-nayanikd@in-nayanikd.local>
2018-08-28 16:46:38 +0530

    Runs per over in sync with each run recorded

--- src/currentDelivery/ScoreInput.jsx
+++ src/currentDelivery/ScoreInput.jsx
@@ -3,6 +3,7 @@ import Button from 'reactstrap/lib/Button';
 import { connect } from 'react-redux';
 import { updateRuns, resetCurrentDelivery } from '../currentDelivery/currentDeliveryReducer';
 import { updateInningsScore, createInitialInning } from '../innings/inningsReducer'
+import { updateRunsPerOver } from '../scorer/currentOverStatsReducer';
 
 const SCORES_POSSIBLE = [0, 1, 2, 3, 4, 5, 6, 7];
 
@@ -22,6 +23,7 @@ const ScoreInput = (props) => (
     <Button onClick={() => {
         props.updateDelivery(props.runs);
         props.resetCurrentDelivery();
+        props.updateRunsPerOver(props.runs);
       }}
     > Next Ball </Button>
   </div>
@@ -33,6 +35,7 @@ const mapStateAsProps = (state) => ({
 
 const mapDispatchAsProps = (dispatch) => ({
   onSelectRuns: (runs) => dispatch(updateRuns(runs)),
+  updateRunsPerOver: (runs) => dispatch(updateRunsPerOver(runs)),
   resetCurrentDelivery: () => dispatch(resetCurrentDelivery()),
   updateDelivery: (runs) => dispatch(updateInningsScore(runs)),
   createInnings : () => dispatch(createInitialInning()),
