cbe3851873d3b883dec72ca13debe98a2ffb1bf5
ravia91 <rc.chandra91@gmail.com>
2018-08-29 18:29:13 +0530

    Added navigation arrows

--- src/gameDetails/GameDetails.jsx
+++ src/gameDetails/GameDetails.jsx
@@ -3,14 +3,28 @@ import BattingDetails from './BattingDetails.js';
 import BowlingDetails from './BowlingDetails.js';
 import {ConnectedScoreBoard, ConnectedScoredCardForPlayedTeam} from '../scorer/Scoreboard'; 
 import { Container, Row, Col } from 'reactstrap';
+import { HashLink as Link } from 'react-router-hash-link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons';
 
 const GameDetails = () => (
   <Container>
-    <Row>
+     <Row>
       <Col md={{ size: 6, offset: 3 }} className='background-app-color'>
+        <div className='text-left'>
+          <Link to="/scorer" className='link-arrow'>
+            <FontAwesomeIcon icon={faArrowCircleLeft} />
+          </Link>
+        </div>
+
+
+      </Col>
+    </Row>
+    <Row>
+      <Col md={{ size: 6, offset: 3 }} className='background-app-color view-height-100'>
         <br/>
     <ConnectedScoreBoard />
-    <ConnectedScoredCardForPlayedTeam />
+    <ConnectedScoredCardForPlayedTeam /><br/>
     <BattingDetails /><br/><br/>
     <BowlingDetails />
   </Col>
