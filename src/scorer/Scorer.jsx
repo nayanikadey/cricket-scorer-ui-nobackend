46370cdc034c6513e47e8c0ac93446e3409fd81c
Bhupinder Singh <bhupisgh@thoughtworks.com>
2018-08-29 19:37:09 +0530

    Arrows fix

--- src/scorer/Scorer.jsx
+++ src/scorer/Scorer.jsx
@@ -3,35 +3,20 @@ import ConnectedCurrentOverStats from "../currentOverStats/CurrentOverStats";
 import ScoreInput from '../currentDelivery/ScoreInput';
 import { ConnectedScoredCardForPlayedTeam, ConnectedScoreBoard } from './Scoreboard';
 import { Container, Row, Col } from 'reactstrap';
-import { HashLink as Link } from 'react-router-hash-link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
 import BowlingModal from '../modal/BowlerModal';
 
 const Scorer = () => (
   <Container>
     <Row>
-      <Col md={{ size: 6, offset : 3}} className='background-app-color'>
-      <div className='text-right'>
-        <Link to="/gameDetails" className='link-arrow'>
-          <FontAwesomeIcon icon={faArrowCircleRight} />
-        </Link>
-      </div>
-        
-        
-      </Col>
-    </Row>
-    <Row>
       <Col md={{ size: 6, offset: 3 }} className='background-app-color view-height-100'>
         <br/>
-        <ConnectedScoreBoard />
+        <ConnectedScoreBoard pageName='scorer'/>
         <ConnectedScoredCardForPlayedTeam />
         <ConnectedCurrentOverStats />
         <ScoreInput />
         <BowlingModal />
       </Col>
     </Row>
-
   </Container>
 );
 
