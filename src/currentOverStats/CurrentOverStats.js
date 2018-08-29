ada86354fd424032d0bb1f1b9a005be864efa423
Bhupinder Singh <bhupisgh@thoughtworks.com>
2018-08-29 13:08:32 +0530

    CSS fix for Story 2

--- src/currentOverStats/CurrentOverStats.js
+++ src/currentOverStats/CurrentOverStats.js
@@ -1,32 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container, Row, Col } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import PropTypes from 'prop-types';
 import CricketUtility from '../Utility/cricketUtility';
 
 
 const CurrentOverStats = props => (
-  <Container>
+  <div>
     <br />
     <Row>
-      <Col md={{ size: 3, offset: 3 }}>
-        <Row>
-          <Col md="5" xs="4">
-            This Over
-          </Col>
-          <Col sm="1" xs="2" />
-          <Col style={{ textAlign: 'left' }}>
-            {props.runsPerOver.join(' ')}
-          </Col>
-        </Row>
+      <Col md={{ size: 4 }}>
+        This Over
+      </Col>
+      <Col style={{ textAlign: 'left' }}>
+        {props.runsPerOver.join(' ')}
       </Col>
     </Row>
     <Row>
-      <Col md={{ size: 3, offset: 3 }} sm="12">
+      <Col>
         Bowler: {props.bowlerName}
       </Col>
     </Row>
-  </Container>
+  </div>
 );
 
 CurrentOverStats.propTypes = {
